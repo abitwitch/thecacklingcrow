e006abf834ab357a96181b320e40ed8dae8bbffb
abitwitch <arrakis256@gmail.com>
2023-08-30 20:16:55 +0100

    Wed 30 Aug 20:16:55 BST 2023

--- update.sh
+++ update.sh
@@ -1,32 +1,29 @@
+#!/bin/bash
+
 cd "$(dirname "$0")";
 
 curdir=$(pwd | xargs basename)
 gitreponame=$(basename `git rev-parse --show-toplevel`)
 
-lastmod=$(date -r /var/www/html +%s)
-if [ ! -f ./lastrun ]; then
-    echo 0 > ./lastrun
-fi
-lastrun=$(cat ./lastrun)
-
 if [ "$curdir" = "$gitreponame" ]; then
-    if [ "$lastmod" -gt "$lastrun" ]; then
-        #Clean git repo
-        sudo find ./ -mindepth 1 ! -regex '^./.git\(/.*\)?' | grep -v '^./.github*' | grep -v './clean.sh' | grep -v './update.sh'  | grep -v './README.md' | grep -v './lastrun' | grep -v './.gitignore' | xargs sudo rm -rf
-        #Clean prev run of static site
-        sudo rm -r /var/www/html/crow.local-static/
-        #Create static version of site
-        cd /var/www/html
-        yes | sudo ./wpstatic -a
-        cd -
-        #Copy static site to git repo
-        sudo cp /var/www/html/crow.local-static/* . -r
+    #Clean git repo
+    sudo find ./ -mindepth 1 ! -regex '^./.git\(/.*\)?' | grep -v '^./.github*' | grep -v './clean.sh' | grep -v './update.sh'  | grep -v './README.md' | grep -v './lastrun' | grep -v './.gitignore' | xargs sudo rm -rf
+    #Clean prev run of static site
+    sudo rm -r /var/www/html/crow.local-static/
+    #Create static version of site
+    cd /var/www/html
+    yes | sudo ./wpstatic -a
+    cd -
+    #Copy static site to git repo
+    sudo cp /var/www/html/crow.local-static/* . -r
+    gitdiff=$(git diff --ignore-all-space)
+    if [ "$gitdiff" != "" ]; then
+        #Update lastrun
+        date +%s > ./lastrun
         #Git commit and push
         sudo -u crow git add -A
         sudo -u crow git commit -m "`date`"
         sudo -u crow git push
-        #Update lastrun
-        date +%s > ./lastrun
     else
         echo "No changes since last run."
     fi
