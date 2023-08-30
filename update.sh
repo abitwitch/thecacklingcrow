5c8b8d8f61b6caed5b91e3a59a0d7f723112dfb2
Crow <thecacklingcrowblog@gmail.com>
2023-08-30 02:55:39 +0100

    Wed 30 Aug 02:55:39 BST 2023

--- update.sh
+++ update.sh
@@ -5,7 +5,9 @@ gitreponame=$(basename `git rev-parse --show-toplevel`)
 
 if [ "$curdir" = "$gitreponame" ]; then
     cd /var/www/html
+    sudo sed -i 's|http://crow.local|https://thecacklingcrow.com|g' wp-config.php
     yes | sudo ./wpstatic
+    sudo sed -i 's|http://thecacklingcrow.com|https://crow.local|g' wp-config.php
     cd -
     sudo cp /var/www/html/crow.local-static/* . -r
     git add -A
