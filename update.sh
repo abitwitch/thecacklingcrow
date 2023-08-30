457c2f716ddd91fa13f7b6c3f61efd06f7468977
abitwitch <arrakis256@gmail.com>
2023-08-30 15:49:34 +0100

    Wed 30 Aug 15:49:34 BST 2023

--- update.sh
+++ update.sh
@@ -1,5 +1,3 @@
-
-
 curdir=$(pwd | xargs basename)
 gitreponame=$(basename `git rev-parse --show-toplevel`)
 
@@ -8,20 +6,20 @@ if [ "$curdir" = "$gitreponame" ]; then
     sudo find ./ -mindepth 1 ! -regex '^./.git\(/.*\)?' | grep -v '^./.github*' | grep -v './clean.sh' | grep -v './update.sh'  | grep -v './README.md' | grep -v './.gitignore' | xargs sudo rm -rf
     #Re-config site URL
     cd /var/www/html
-    sudo sed -i 's|http://crow.local|http://thecacklingcrow.com|g' wp-config.php
+    #sudo sed -i 's|http://crow.local|http://thecacklingcrow.com|g' wp-config.php
     #Clean prev run of static site
     sudo rm -r /var/www/html/crow.local-static/
     #Create static version of site
-    #yes | sudo ./wpstatic
-    cd -
-    wget --recursive --no-clobber --page-requisites --html-extension --convert-links --restrict-file-names=windows crow.local
-    cd /var/www/html
+    yes | sudo ./wpstatic
+    #cd -
+    #sudo wget --recursive --no-clobber --page-requisites --html-extension --convert-links --restrict-file-names=windows crow.local
+    #cd /var/www/html
     #Revert site URL
-    sudo sed -i 's|http://thecacklingcrow.com|http://crow.local|g' wp-config.php
+    #sudo sed -i 's|http://thecacklingcrow.com|http://crow.local|g' wp-config.php
     #Copy static site to git repo
     cd -
-    #sudo cp /var/www/html/crow.local-static/* . -r
-    sudo cp ./crow.local/* . -r
+    sudo cp /var/www/html/crow.local-static/* . -r
+    #sudo cp ./crow.local/* . -r
     #Git commit and push
     sudo -u crow git add -A
     sudo -u crow git commit -m "`date`"
@@ -30,4 +28,3 @@ else
     echo "Error: Set current directory to the repo and then rerun."
     exit 1
 fi
-
