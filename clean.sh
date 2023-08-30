ea8859df2457890093b3537e1ae61d9ee07330da
abitwitch <arrakis256@gmail.com>
2023-08-30 16:23:45 +0100

    Wed 30 Aug 16:23:45 BST 2023

--- clean.sh
+++ clean.sh
@@ -4,7 +4,7 @@ curdir=$(pwd | xargs basename)
 gitreponame=$(basename `git rev-parse --show-toplevel`)
 
 if [ "$curdir" = "$gitreponame" ]; then
-    sudo find ./ -mindepth 1 ! -regex '^./.git\(/.*\)?' | grep -v '^./.github*' | grep -v './clean.sh' | grep -v './update.sh'  | grep -v './README.md' | grep -v './.gitignore' | xargs sudo rm -rf
+    sudo find ./ -mindepth 1 ! -regex '^./.git\(/.*\)?' | grep -v '^./.github*' | grep -v './clean.sh' | grep -v './update.sh'  | grep -v './README.md' | grep -v './lastrun' | grep -v './.gitignore' | xargs sudo rm -rf
 else
     echo "Error: Set current directory to the repo and then rerun."
     exit 1
--- update.sh
+++ update.sh
@@ -1,29 +1,31 @@
 curdir=$(pwd | xargs basename)
 gitreponame=$(basename `git rev-parse --show-toplevel`)
 
+lastmod=$(date -r /var/www/html +%s)
+if [ ! -f ./lastrun ]; then
+    echo 0 > ./lastrun
+fi
+lastrun=$(cat ./lastrun)
+
 if [ "$curdir" = "$gitreponame" ]; then
-    #Clean git repo
-    sudo find ./ -mindepth 1 ! -regex '^./.git\(/.*\)?' | grep -v '^./.github*' | grep -v './clean.sh' | grep -v './update.sh'  | grep -v './README.md' | grep -v './.gitignore' | xargs sudo rm -rf
-    #Re-config site URL
-    cd /var/www/html
-    #sudo sed -i 's|http://crow.local|http://thecacklingcrow.com|g' wp-config.php
-    #Clean prev run of static site
-    sudo rm -r /var/www/html/crow.local-static/
-    #Create static version of site
-    yes | sudo ./wpstatic -a
-    #cd -
-    #sudo wget --recursive --no-clobber --page-requisites --html-extension --convert-links --restrict-file-names=windows crow.local
-    #cd /var/www/html
-    #Revert site URL
-    #sudo sed -i 's|http://thecacklingcrow.com|http://crow.local|g' wp-config.php
-    #Copy static site to git repo
-    cd -
-    sudo cp /var/www/html/crow.local-static/* . -r
-    #sudo cp ./crow.local/* . -r
-    #Git commit and push
-    sudo -u crow git add -A
-    sudo -u crow git commit -m "`date`"
-    sudo -u crow git push
+    if [ "$lastmod" -gt "$lastrun" ]; then
+        #Clean git repo
+        sudo find ./ -mindepth 1 ! -regex '^./.git\(/.*\)?' | grep -v '^./.github*' | grep -v './clean.sh' | grep -v './update.sh'  | grep -v './README.md' | grep -v './lastrun' | grep -v './.gitignore' | xargs sudo rm -rf
+        #Clean prev run of static site
+        sudo rm -r /var/www/html/crow.local-static/
+        #Create static version of site
+        cd /var/www/html
+        yes | sudo ./wpstatic -a
+        cd -
+        #Copy static site to git repo
+        sudo cp /var/www/html/crow.local-static/* . -r
+        #Git commit and push
+        sudo -u crow git add -A
+        sudo -u crow git commit -m "`date`"
+        sudo -u crow git push
+        #Update lastrun
+        date +%s > ./lastrun
+    fi
 else
     echo "Error: Set current directory to the repo and then rerun."
     exit 1
